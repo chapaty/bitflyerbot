839a388833b71ea36f78c9d8120c84a26b1d68a3
chapaty <beatboxer211@gmail.com>
2017-04-25 15:55:55 +0900

    test

---
+++ app.js
@@ -0,0 +1,13 @@
+﻿console.log('Loading');
+
+exports.handler = function (event, context, callback) {
+
+    if (event != null) {
+        console.log('event = ' + JSON.stringify(event));
+    }
+    else {
+        console.log('No event object');
+    }
+
+    callback(null, 'Hello World');  // SUCCESS with message
+};
