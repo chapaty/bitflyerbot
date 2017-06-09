935a0853209c9eb60dcc309562622cbad25548e9
chapaty <beatboxer211@gmail.com>
2017-06-09 20:18:48 +0900

    数値DB保存機能実装

---
+++ app (2).js
@@ -0,0 +1,12 @@
+﻿
+exports.handler = function (event, context, callback) {
+
+    if (event != null) {
+        console.log('event = ' + JSON.stringify(event));
+    }
+    else {
+        console.log('No event object');
+    }
+
+    callback(null, 'Hello World');  // SUCCESS with message
+};
--- app.js
+++ app.js
@@ -1,13 +1,44 @@
-﻿console.log('Loading');
+﻿var request = require('request');
+const async = require("async");
 
-exports.handler = function (event, context, callback) {
 
-    if (event != null) {
-        console.log('event = ' + JSON.stringify(event));
-    }
-    else {
-        console.log('No event object');
-    }
+﻿var AWS = require('aws-sdk');
+var dynamo = new AWS.DynamoDB.DocumentClient();
+var tableName = "bitflyer_history";
 
-    callback(null, 'Hello World');  // SUCCESS with message
+exports.handler = function (event, context) {
+    var path = '/v1/ticker';
+    var query = '?product_code=FX_BTC_JPY';
+    var url = 'https://api.bitflyer.jp' + path + query;
+
+    async.waterfall([
+        function (next) {
+            request(url, function (err, response, payload) {
+                console.log(JSON.parse(payload));
+                next(null, JSON.parse(payload));
+            });
+        },
+        function (data, next) {
+            console.log(data);
+            var params = {
+                TableName: tableName,
+                Item: {
+                    "date_id": data.timestamp,
+                    "price" : data.ltp
+                }
+            }
+            console.log(params);
+            dynamo.put(params, function (err, data) {
+                if (err) {
+                    console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+                } else {
+                    console.log("Added item:", JSON.stringify(data, null, 2));
+                }
+                next(null);
+            });
+        }
+    ], function (err, res) {
+        context.done(null, res);  // SUCCESS with message
+    });
+  
 };
